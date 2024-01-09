6068a56e3aacb5b47d94da4c0abbc761285f8dd7
TW - IT - BE - Willy Cheng <willy.cheng@shoalter.com>
2024-01-09 12:26:09 +0800

    add jira code

--- shoalter-tools/src/main/java/com/shoalter/willy/shoaltertools/config/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.shoalter.willy.shoaltertools.config;
-
-public class Constants {
-  public static final String MQ_PRODUCT_INFO_ROUTING_KEY =
-      "shoalter-see-product-master.product-info-iids";
-}
